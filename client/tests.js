f29b177379627f8d0ee7f9184da08caaa92b397b
Maharudin <62484722+b4tu91ok@users.noreply.github.com>
2020-03-31 03:35:50 +0700

    Update tests.js

--- client/tests.js
+++ client/tests.js
@@ -3,7 +3,7 @@ import 'angular-mocks';
 import moment from 'moment';
 import './';
 
-moment.tz.setDefault('Australia/Sydney');
+moment.tz.setDefault('Asia/Jakarta');
 var testsContext = require.context('.', true, /_test.[j|t]sx?$/);
 
 testsContext.keys().forEach(testsContext);
